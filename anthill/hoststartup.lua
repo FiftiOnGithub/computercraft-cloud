10caf36bc5a0d6686a25d083a9a891d99c5cec77
Fifti <gregory.tom@outlook.com>
2025-12-27 20:36:09 +0200

    Delete anthill directory

--- anthill/hoststartup.lua
+++
@@ -1,6 +0,0 @@
-peripheral.call("monitor_3", "setTextScale", 0.5)
-peripheral.call("monitor_1", "setTextScale", 0.5)
-
-multishell.launch(_ENV, "rom/programs/monitor.lua", "monitor_1", "anthill/host.lua")
-multishell.launch(_ENV, "rom/programs/monitor.lua", "monitor_3", "anthill/tracker.lua")
-multishell.launch(_ENV, "anthill/hostlog.lua")
